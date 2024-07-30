36cc96b79565dd8dec8d42be1d33e75c95ce51e2
waifod <dumatte@amazon.com>
2024-07-30 19:36:41 +0200

    Add spheres, gradient and draw

--- main.cpp
+++ main.cpp
@@ -1,22 +1,74 @@
+#include <cmath>
 #include <iostream>
 
 #include "src/color.hpp"
+#include "src/ray.hpp"
+#include "src/vec3.hpp"
+
+// Image
+constexpr double ASPECT_RATIO {16.0/9.0};
+constexpr int IMAGE_HEIGHT {800};
+
+constexpr int getImageWidth() {
+    auto width {int(double(IMAGE_HEIGHT) * ASPECT_RATIO)};
+    return width < 1 ? 1 : width;
+}
+constexpr int IMAGE_WIDTH {getImageWidth()};
+
+// Camera
+constexpr double FOCAL_LENGTH {1.0};
+constexpr double VIEWPORT_HEIGHT {2.0};
+constexpr double VIEWPORT_WIDTH {VIEWPORT_HEIGHT * (double(IMAGE_WIDTH) / IMAGE_HEIGHT)};
+constexpr point CAMERA_CENTER {point(0,0,0)};
+
+double hit_sphere(const point& center, double radius, const ray& r) {
+    auto oc_dir {center - r.origin()};
+    auto a {norm(r.direction())};
+    auto h {dot(r.direction(), oc_dir)};
+    auto c {norm(oc_dir) - radius*radius};
+    auto discriminant {h*h - a*c};
+
+    if (discriminant < 0) {
+        return -1.0;
+    }
+    return (-h-std::sqrt(discriminant)) / a;
+}
+
+color ray_color(const ray& r) { 
+    auto t {hit_sphere(point(0,0,-1), 0.5, r)};
+    if (t > 0.0) {
+        auto N {unit_vector(r.at(t)-vec3(0,0,-1))};
+        return color(N.x()+1, N.y()+1, N.z()+1) / 2;
+    } 
+    auto unit_direction {unit_vector(r.direction())};
+    auto a {(unit_direction.y()+1.0)/2};
+    return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
+}
 
 int main() {
+    // Calculate the vectors across the horizontal and down the vertical viewport edges.
+    auto viewport_u {vec3(VIEWPORT_WIDTH, 0, 0)};
+    auto viewport_v {vec3(0, -VIEWPORT_HEIGHT, 0)};
 
-    // Image
+    // Calculate the horizontal and vertical delta vectors from pixel to pixel.
+    auto pixel_delta_u {viewport_u / IMAGE_WIDTH};
+    auto pixel_delta_v {viewport_v / IMAGE_HEIGHT};
 
-    int image_width = 256;
-    int image_height = 256;
-
+    // Calculate the location of the upper left pixel.
+    auto viewport_upper_left {CAMERA_CENTER - vec3(0,0,FOCAL_LENGTH) - viewport_u/2 - viewport_v/2};
+    auto pixel00_loc {viewport_upper_left + (pixel_delta_u + pixel_delta_v)/2};
+    
     // Render
 
-    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    std::cout << "P3\n" << IMAGE_WIDTH << ' ' << IMAGE_HEIGHT << "\n255\n";
 
-    for (int j = 0; j < image_height; j++) {
-        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
-        for (int i = 0; i < image_width; i++) {
-            auto pixel_color {color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0.0)};
+    for (int j = 0; j < IMAGE_HEIGHT; j++) {
+        std::clog << "\rScanlines remaining: " << (IMAGE_HEIGHT - j) << ' ' << std::flush;
+        for (int i = 0; i < IMAGE_WIDTH; i++) {
+            auto pixel_center {pixel00_loc + i*pixel_delta_u + j*pixel_delta_v};
+            auto ray_direction {pixel_center - CAMERA_CENTER};
+            auto r {ray(CAMERA_CENTER, ray_direction)};
+            auto pixel_color {ray_color(r)};
             
             write_color(std::cout, pixel_color);
         }
--- src/vec3.hpp
+++ src/vec3.hpp
@@ -5,43 +5,43 @@
 
 class vec3 {
   public:
-    explicit vec3() noexcept = default;
-    explicit vec3(double x, double y, double z) noexcept;
+    constexpr explicit vec3() noexcept = default;
+    constexpr explicit vec3(double x, double y, double z) noexcept;
 
-    double x() const noexcept;
-    double y() const noexcept;
-    double z() const noexcept;
+    constexpr double x() const noexcept;
+    constexpr double y() const noexcept;
+    constexpr double z() const noexcept;
 
-    vec3& operator+=(const vec3& other) noexcept;
-    vec3& operator-=(const vec3& other) noexcept;
-    vec3& operator*=(double t) noexcept;
-    vec3& operator/=(double t);
+    constexpr vec3& operator+=(const vec3& other) noexcept;
+    constexpr vec3& operator-=(const vec3& other) noexcept;
+    constexpr vec3& operator*=(double t) noexcept;
+    constexpr vec3& operator/=(double t);
   
-    double& operator[](int i) noexcept;
-    double operator[](int i) const noexcept;
+    constexpr double& operator[](int i) noexcept;
+    constexpr double operator[](int i) const noexcept;
 
   private:
     std::array<double, 3> m_c{0,0,0};
 };
 
-vec3 operator+(const vec3& u, const vec3& v) noexcept;
+constexpr vec3 operator+(const vec3& u, const vec3& v) noexcept;
 
-vec3 operator-(const vec3& u) noexcept;
-vec3 operator-(const vec3& u, const vec3& v) noexcept;
+constexpr vec3 operator-(const vec3& u) noexcept;
+constexpr vec3 operator-(const vec3& u, const vec3& v) noexcept;
 
-vec3 operator*(const vec3& u, double t) noexcept;
-vec3 operator*(double t, const vec3& u) noexcept;
-vec3 operator*(const vec3& u, const vec3& v) noexcept;
+constexpr vec3 operator*(const vec3& u, double t) noexcept;
+constexpr vec3 operator*(double t, const vec3& u) noexcept;
+constexpr vec3 operator*(const vec3& u, const vec3& v) noexcept;
 
-vec3 operator/(const vec3& u, double t);
+constexpr vec3 operator/(const vec3& u, double t);
     
-double dot(const vec3& u, const vec3& v) noexcept;
-vec3 cross(const vec3& u, const vec3& v) noexcept;
+constexpr double dot(const vec3& u, const vec3& v) noexcept;
+constexpr vec3 cross(const vec3& u, const vec3& v) noexcept;
     
-double norm(const vec3& u) noexcept;
-double norm_squared(const vec3& u) noexcept;
+constexpr double norm(const vec3& u) noexcept;
+constexpr double norm_squared(const vec3& u) noexcept;
 
-vec3 unit_vector(const vec3& u);
+constexpr vec3 unit_vector(const vec3& u);
 
 std::ostream& operator<<(std::ostream& out, const vec3& u);
 
