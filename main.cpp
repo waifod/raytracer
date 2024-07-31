56a1cd4b3a0f69e6475f00e38234f2d2e31bc72b
waifod <dumatte@amazon.com>
2024-07-31 10:27:05 +0200

    Refactor vec3 and use static constexpr

--- main.cpp
+++ main.cpp
@@ -23,9 +23,9 @@ constexpr point CAMERA_CENTER {point(0,0,0)};
 
 double hit_sphere(const point& center, double radius, const ray& r) {
     auto oc_dir {center - r.origin()};
-    auto a {norm(r.direction())};
+    auto a {r.direction().norm()};
     auto h {dot(r.direction(), oc_dir)};
-    auto c {norm(oc_dir) - radius*radius};
+    auto c {oc_dir.norm() - radius*radius};
     auto discriminant {h*h - a*c};
 
     if (discriminant < 0) {
@@ -34,30 +34,30 @@ double hit_sphere(const point& center, double radius, const ray& r) {
     return (-h-std::sqrt(discriminant)) / a;
 }
 
-color ray_color(const ray& r) { 
+color ray_color(const ray& r) {
     auto t {hit_sphere(point(0,0,-1), 0.5, r)};
     if (t > 0.0) {
-        auto N {unit_vector(r.at(t)-vec3(0,0,-1))};
+        auto N {(r.at(t)-vec3(0,0,-1)).unit_vector()};
         return color(N.x()+1, N.y()+1, N.z()+1) / 2;
-    } 
-    auto unit_direction {unit_vector(r.direction())};
+    }
+    auto unit_direction {r.direction().unit_vector()};
     auto a {(unit_direction.y()+1.0)/2};
     return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
 }
 
 int main() {
     // Calculate the vectors across the horizontal and down the vertical viewport edges.
-    auto viewport_u {vec3(VIEWPORT_WIDTH, 0, 0)};
-    auto viewport_v {vec3(0, -VIEWPORT_HEIGHT, 0)};
+    static constexpr auto viewport_u {vec3(VIEWPORT_WIDTH, 0, 0)};
+    static constexpr auto viewport_v {vec3(0, -VIEWPORT_HEIGHT, 0)};
 
     // Calculate the horizontal and vertical delta vectors from pixel to pixel.
-    auto pixel_delta_u {viewport_u / IMAGE_WIDTH};
-    auto pixel_delta_v {viewport_v / IMAGE_HEIGHT};
+    static constexpr auto pixel_delta_u {viewport_u / IMAGE_WIDTH};
+    static constexpr auto pixel_delta_v {viewport_v / IMAGE_HEIGHT};
 
     // Calculate the location of the upper left pixel.
-    auto viewport_upper_left {CAMERA_CENTER - vec3(0,0,FOCAL_LENGTH) - viewport_u/2 - viewport_v/2};
-    auto pixel00_loc {viewport_upper_left + (pixel_delta_u + pixel_delta_v)/2};
-    
+    static constexpr auto viewport_upper_left {CAMERA_CENTER - vec3(0,0,FOCAL_LENGTH) - viewport_u/2 - viewport_v/2};
+    static constexpr auto pixel00_loc {viewport_upper_left + (pixel_delta_u + pixel_delta_v)/2};
+
     // Render
 
     std::cout << "P3\n" << IMAGE_WIDTH << ' ' << IMAGE_HEIGHT << "\n255\n";
@@ -69,10 +69,10 @@ int main() {
             auto ray_direction {pixel_center - CAMERA_CENTER};
             auto r {ray(CAMERA_CENTER, ray_direction)};
             auto pixel_color {ray_color(r)};
-            
+
             write_color(std::cout, pixel_color);
         }
     }
 
-    std::clog << "\rDone.\n";
+    std::clog << "\nDone.\n";
 }
--- src/vec3.hpp
+++ src/vec3.hpp
@@ -2,48 +2,94 @@
 #define VEC3_H
 
 #include <array>
+#include <cmath>
+#include <iostream>
 
 class vec3 {
-  public:
-    constexpr explicit vec3() noexcept = default;
-    constexpr explicit vec3(double x, double y, double z) noexcept;
-
-    constexpr double x() const noexcept;
-    constexpr double y() const noexcept;
-    constexpr double z() const noexcept;
-
-    constexpr vec3& operator+=(const vec3& other) noexcept;
-    constexpr vec3& operator-=(const vec3& other) noexcept;
-    constexpr vec3& operator*=(double t) noexcept;
-    constexpr vec3& operator/=(double t);
-  
-    constexpr double& operator[](int i) noexcept;
-    constexpr double operator[](int i) const noexcept;
-
-  private:
+   private:
     std::array<double, 3> m_c{0,0,0};
+
+  public:
+    [[nodiscard]] constexpr explicit vec3() noexcept = default;
+    [[nodiscard]] constexpr explicit vec3(double x, double y, double z) noexcept
+        : m_c{x,y,z} {}
+
+    [[nodiscard]] constexpr double x() const noexcept { return m_c[0]; }
+    [[nodiscard]] constexpr double y() const noexcept { return m_c[1]; }
+    [[nodiscard]] constexpr double z() const noexcept { return m_c[2]; }
+
+    constexpr vec3& operator+=(const vec3& other) noexcept {
+      m_c[0] += other.x();
+      m_c[1] += other.y();
+      m_c[2] += other.z();
+      return *this;
+    }
+
+    [[nodiscard]] constexpr vec3 operator+(const vec3& other) const noexcept {
+      return vec3(x()+other.x(), y()+other.y(), z()+other.z());
+    }
+
+    constexpr vec3& operator-=(const vec3& other) noexcept { return *this += -other; }
+
+    [[nodiscard]] constexpr vec3 operator-() const noexcept { return vec3(-m_c[0], -m_c[1], -m_c[2]); }
+
+    [[nodiscard]] constexpr vec3 operator-(const vec3& other) const noexcept {
+      return *this + (-other);
+    }
+
+    constexpr vec3& operator*=(double t) noexcept {
+      m_c[0] *= t;
+      m_c[1] *= t;
+      m_c[2] *= t;
+      return *this;
+    }
+
+    constexpr vec3 operator*(double t) const noexcept {
+      return vec3(x()*t, y()*t, z()*t);
+    }
+
+    constexpr vec3 operator*(const vec3& v) const noexcept {
+      return vec3(x() * v.x(), y() * v.y(), z() * v.z());
+    }
+
+    constexpr vec3& operator/=(double t) {
+      return *this *= 1/t;
+    }
+
+  constexpr vec3 operator/(double t) const {
+      return *this * (1/t);
+  }
+
+  [[nodiscard]] constexpr double norm() const noexcept {
+    return std::sqrt(norm_squared());
+  }
+  [[nodiscard]] constexpr double norm_squared() const noexcept {
+    return x()*x() + y()*y() + z()*z();
+  }
+
+  [[nodiscard]] constexpr vec3 unit_vector() const {
+    return *this / norm();
+  }
+    [[nodiscard]] constexpr double& operator[](int i) noexcept { return m_c[i]; }
+    [[nodiscard]] constexpr double operator[](int i) const noexcept { return m_c[i]; }
 };
 
-constexpr vec3 operator+(const vec3& u, const vec3& v) noexcept;
+constexpr vec3 operator*(double t, const vec3& v) noexcept {
+    return v*t;
+}
 
-constexpr vec3 operator-(const vec3& u) noexcept;
-constexpr vec3 operator-(const vec3& u, const vec3& v) noexcept;
+[[nodiscard]] constexpr double dot(const vec3& u, const vec3& v) noexcept {
+    return u.x()*v.x() + u.y()*v.y() + u.z()*v.z();
+}
+[[nodiscard]] constexpr vec3 cross(const vec3& u, const vec3& v) noexcept {
+    return vec3(u.y() * v.z() - u.z() * v.y(),
+                u.z() * v.x() - u.x() * v.z(),
+                u.x() * v.y() - u.y() * v.x());
+}
 
-constexpr vec3 operator*(const vec3& u, double t) noexcept;
-constexpr vec3 operator*(double t, const vec3& u) noexcept;
-constexpr vec3 operator*(const vec3& u, const vec3& v) noexcept;
-
-constexpr vec3 operator/(const vec3& u, double t);
-    
-constexpr double dot(const vec3& u, const vec3& v) noexcept;
-constexpr vec3 cross(const vec3& u, const vec3& v) noexcept;
-    
-constexpr double norm(const vec3& u) noexcept;
-constexpr double norm_squared(const vec3& u) noexcept;
-
-constexpr vec3 unit_vector(const vec3& u);
-
-std::ostream& operator<<(std::ostream& out, const vec3& u);
+inline std::ostream& operator<<(std::ostream& out, const vec3& v) {
+  return out << v.x() << ' ' << v.y() << ' ' << v.z();
+}
 
 using point = vec3;
 
